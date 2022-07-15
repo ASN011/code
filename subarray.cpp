ca4210047cb0cd7cc76b17bf71a89737840c10ed
Adityasaini10012001 <72370777+Adityasaini10012001@users.noreply.github.com>
2022-07-15 18:08:11 +0530

    Add files via upload

---
+++ subarray.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,k;
+	    cin>>n>>k;
+	    if(n>1 && k==1)
+	    {
+	        cout<<"-1"<<endl;
+	        continue;
+	    }
+	    else
+	    {
+	        vector<int>v(n) ;
+	        iota(v.begin(),v.end(), 1);
+	        reverse(v.end()-(n-k+1),v.end());
+	        for(int x:v)
+	        {
+	            cout<<x<<' ';
+	        }
+	        cout<<endl;
+	    }
+	}
+	return 0;
+}
